1c74499ac4643b3a0ad999c2a038d26f30249fc5
a-poor <45295232+a-poor@users.noreply.github.com>
2020-10-10 20:00:41 -0400

    Bump version: 0.0.2 → 0.0.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="qgates",
-    version="0.0.2",
+    version="0.0.3",
     author="Austin Poor",
     author_email="austinpoor@gmail.com",
     description="Helper library for quantum matrix math",
